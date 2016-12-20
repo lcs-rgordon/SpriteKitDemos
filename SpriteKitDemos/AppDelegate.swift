0a63cc334fe76c46e5214ac5c781752e233b44d5
Russell Gordon <rgordon@rsgc.on.ca>
2016-12-20 04:37:03 -0500

    Initial Commit

---
+++ SpriteKitDemos/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  SpriteKitDemos
+//
+//  Created by Russell Gordon on 12/20/16.
+//  Copyright © 2016 Russell Gordon. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
